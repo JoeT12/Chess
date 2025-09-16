a097d26e86ec342c39fa7babeff9e473eafe05f4
JoeT12 <108418412+JoeT12@users.noreply.github.com>
2025-09-16 20:36:31 +0100

    Onboarding Platform Onto Kubernetes (#34)
    * Added Dockerfiles + Helm Charts To All Microservices. Built Umbrella Helm Chart To Deploy Entire Chess Platform With One Command
    
    * Refactoring Charts
    
    * Update socket.ts

--- c-ui/src/lib/socket.ts
+++ c-ui/src/lib/socket.ts
@@ -2,10 +2,13 @@ import { io, Socket } from "socket.io-client";
 import { config } from "../config";
 
 let socket: Socket | null = null;
+let host = config.env === "development" ? "http://localhost:8082" : "https://chess.local";
+let path = config.env === "development" ? "/socket.io" : "/game/socket.io";
 
 export function getSocket(accessToken: string) {
   if (!socket) {
-    socket = io(`${config.gameServerHost}`, {
+    socket = io(host, {
+      path: path, 
       autoConnect: false,
       reconnection: false,
       query: { token: accessToken },
